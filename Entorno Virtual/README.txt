239a9c92bbef069a102b042bb518c7f350a4f006
alejandrochmejia <alejandrochmejia@gmail.com>
2024-05-01 08:29:03 -0400

    Agregado del entorno virtual (Attempt 1)

---
+++ Entorno Virtual/README.txt
@@ -0,0 +1,3 @@
+El archivo "app.py" contenido en
+src fue ejecutado en las versiones
+de Python 3.12.0 y 3.12.3
---
+++ Entorno Virtual/src/app.py
@@ -0,0 +1,14 @@
+#Esto es un pequeño programa de prueba de entorno virtual
+
+class Bicicleta():
+    # Constructor de la clase
+    def __init__( self ):
+        pass
+
+    def caballito( self ):
+        print("La bici hace caballito.")
+    
+    # Fin de la clase Bicicleta
+
+bmx = Bicicleta()
+bmx.caballito()
